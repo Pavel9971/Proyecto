35a706aee26d6021f038b6995a5f6b9f05b894af
David Pavel Tapia Prado <david.tapia.prado@estudiantes.cordillera.edu.ec>
2021-07-26 15:44:21 -0500

    Creacion de los formularios para el usuario

---
+++ Proyecto/Frm_AgregarUsuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto
+{
+    public partial class Frm_AgregarUsuario : Form
+    {
+        public Frm_AgregarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Frm_AgregarUsuario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
