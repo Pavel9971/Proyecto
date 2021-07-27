2ceb916cd26a4ac577bf62be0757e448154197b2
DAYANNA BAUTISTA CAMPOVERDE <dayanna.bautista.campoverde@estudiantes.cordillera.edu.ec>
2021-07-27 00:50:12 -0500

    formulario ventas

--- Proyecto/FrmVentas.Designer.cs
+++ Proyecto/FrmVentas.Designer.cs
@@ -29,10 +29,17 @@ namespace Proyecto
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FrmVentas";
             this.Text = "FrmVentas";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
